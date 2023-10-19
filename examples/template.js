62046cae50d0db1368133339ebead8a8b231683a
Hamit Mizrak <hamitmizrak@gmail.com>
2023-10-19 14:13:25 +0300

    Dom

--- examples/template.js
+++ examples/template.js
@@ -1,2 +1,20 @@
 /*  */
-window.console.log("loglama")
+const submit_form=()=>{
+    //alert("Çalıştı")
+    // Browser sen dur 
+    //event.preventDefault();
+
+    let username,surname,email,password;
+    username=document.getElementById("uname").value;
+    surname=document.getElementById("usurname").value;
+    email=document.getElementById("uemail").value;
+    password=document.getElementById("upassword").value;
+
+    // Local Storage
+    localStorage.setItem("user_email", email);
+    localStorage.setItem("user_password", password);
+
+    alert(localStorage.getItem("user_email"));
+
+    document.getElementById("result_id").innerHTML="<b>"+username+ "</b>" ;
+}
